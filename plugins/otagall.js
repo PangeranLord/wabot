6cb5577b33926c3cc6a16dec4bbd7d916bc025aa
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-14 02:16:22 +0700

    Update otagall.js

--- plugins/otagall.js
+++ plugins/otagall.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+  m.reply('TAG ALL..!!\n' + text + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
 }
-handler.command = ['tagall']
+handler.help = ['tagall','-'].map(v => 'o' )
 handler.tags = ['owner']
 handler.command = /^(otagall|o\-)$/i
 
