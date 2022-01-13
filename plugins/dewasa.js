d6076cb89d320f7b93309891faa69e4a1cbbe883
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-13 09:33:49 +0700

    Update dewasa.js

--- plugins/dewasa.js
+++ plugins/dewasa.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn }) => {
 	
-	await m.reply('[❗] WAIT, Tunggu Sebentar:v.')
+	await m.reply('[❗] WAIT, Tunggu Sebentar Sayang:v.')
   conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
 }
 handler.help = ['filebokep']
