fec06785194365a3d6c5232a46e4c6410d20f22b
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-13 09:32:10 +0700

    Update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
-  m.reply('Loading...')
+  m.reply('Loading Bentar Sayang...')
   let res = `https://api.ichikaa.xyz/api/randomimage/milf?apikey=79lJ9HEA`
   conn.sendFile(m.chat, res, 'milf.jpg', 'huu suka ama milf', m)
 }
 handler.help = ['milf']
 handler.tags = ['anime']
 handler.limit = true
+handler.premium = true
 
 handler.command = /^(milf)$/i
 
