75379e2dcec6b8a078a2030eb290f36505f7fd47
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-14 03:21:15 +0700

    Update tahta2.js

--- plugins/tahta2.js
+++ plugins/tahta2.js
@@ -15,7 +15,7 @@ handler.tags = ['nulis']
 handler.command = /^tahta2$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = true
 handler.private = false
 
