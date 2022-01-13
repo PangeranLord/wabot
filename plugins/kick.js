2cc5bda65b23718290100540ad32bdc554365c82
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-13 23:10:13 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
 }
 handler.help = ['kick','-'].map(v => 'o' + v + ' @user')
 handler.tags = ['admin']
-handler.command = /^(kick|o\-)$/i
+handler.command = /^(kick\-)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
