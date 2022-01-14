d1c76ede79706abb8599ed2214572450d31dba1b
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-14 14:49:55 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,14 +1,13 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('Done! Bot nya sudah mati di group ini..')
+    m.reply('Done!')
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner','admin']
 handler.command = /^banchat$/i
 handler.owner = true
-handler.group = true
 handler.admin = true
 
 module.exports = handler
